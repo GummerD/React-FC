88c3ab9e02166328f45cab2777188412bbcbb488
GummerD <denis_gummer@mail.ru>
2023-12-11 20:31:00 +0300

    1.2. Две функции increment и decrement.
    - птрогали JSX;
    - назначили двум конпкам функции инкрмента и декримента, которые
    отражаются в консоле, но не выводятся в DOM.
    Это связано с тем, что у каждого элемента есть свое состонияе,
    которое необходимо отражать через хук - useState.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 function App() {
+  let likes = 0;
+
+  function inc(){
+    likes += 1;
+    console.log(likes);
+  }
+
+  function dec(){
+    likes -= 1;
+    console.log(likes);
+  }
+  
   return (
     <div className="App">
-      
+      <h1>{likes}</h1>
+      <button onClick={inc}>Increment</button>
+      <button onClick={dec}>Decrement</button>
     </div>
   );
 }
