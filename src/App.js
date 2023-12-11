c7cd66b9b438d84d532bf6ec510f0a80ad1153f0
GummerD <denis_gummer@mail.ru>
2023-12-11 20:52:08 +0300

    1.4. Двухстороннее связывение, управляемые ком-ты.
    - создали пдополнительный хук, связыли его с полем
    input type=text, чрез строку смогли менять значение в
    h2 {value}

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState('Именяемый текст');
 
   function inc(){
     setLikes(likes + 1);
@@ -14,8 +15,17 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <hr />
       <button onClick={inc}>Increment</button>
       <button onClick={dec}>Decrement</button>
+      <hr />
+      <h2>{value}</h2>
+      <input 
+        type="text" 
+        value={value} 
+        //** двухсторонее связывание, такие компоненты называются управляемыми */
+        onChange={event => setValue(event.target.value)}  
+      />
     </div>
   );
 }
