334d7ba53c6f374ea095508f11948ed826f85d77
GummerD <denis_gummer@mail.ru>
2023-12-11 22:02:00 +0300

    1.5. Немного изучили классы-компоненты.
    - модернизировали духстороннее савязывение через отдельный
    класс-компонент. Это устаревший способ, сейчас все испоотзуют
    функуиональные компоненты.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ClassSetText from "./components/ClassSetText";
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState('Именяемый текст');
-
-  function inc(){
-    setLikes(likes + 1);
-  }
-
-  function dec(){
-    setLikes(likes - 1);
-  }
+ 
+  
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <hr />
-      <button onClick={inc}>Increment</button>
-      <button onClick={dec}>Decrement</button>
-      <hr />
-      <h2>{value}</h2>
-      <input 
-        type="text" 
-        value={value} 
-        //** двухсторонее связывание, такие компоненты называются управляемыми */
-        onChange={event => setValue(event.target.value)}  
-      />
+      <Counter></Counter>
+      <ClassSetText></ClassSetText>
     </div>
   );
 }
---
+++ src/components/ClassSetText.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+class ClassSetText extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "Изменяемый текст",
+        }
+
+        this.setVal = this.setVal.bind(this);
+    }
+
+    setVal(new_value){
+        this.setState({count: this.state.value = new_value })
+    }
+
+
+
+    render() {
+        return (
+            <>
+                <hr />
+                <h2>{this.state.value}</h2><input
+                    type="text"
+                    value={this.state.value}
+                    //** двухсторонее связывание, такие компоненты называются управляемыми */
+                    onChange={event => this.setVal(event.target.value)} />
+            </>
+        )
+    }
+}
+
+export default ClassSetText;
