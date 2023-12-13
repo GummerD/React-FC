9f73885e4d06bdaf70595f5a37ded88fa2be8276
GummerD <denis_gummer@mail.ru>
2023-12-13 17:43:36 +0300

    2.2. UI (button и input), модули css.
    - создали свои input и button, накнули стили;
    - стили накинули через постфиксы .module.css, пример:
    MyInput.module.css.
    - импортировали в корневом эл-те - App, свои input и button;
    - установили связи м передали пропсы в кастомные компоненты:
    input и button.
    - один из input  получает информацию через хук - useRef
    т.е. это неуправляемый эелемент, его советуют не использовать

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PostList from "./components/PostList.jsx";
 import './style/App.css';
+import MyButton from "./components/UI/button/MyButton.jsx";
+import MyInput from "./components/UI/input/MyInput.jsx";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -21,12 +23,36 @@ function App() {
     { id: 6, title: "Some title 6", description: "Some description 6" },
   ]);
 
+  const [title, setTitle] = useState('');
+  const bodyInputRef = useRef();
 
+  function addNewPost(event) {
+    event.preventDefault()
+    console.log(title)
+    console.log(bodyInputRef.current.value)
+
+  }
 
   return (
     <div className="App">
-      <PostList posts = {posts} title = {'The posts list 1'}/>
-      <PostList posts = {posts_2} title = {'The posts list 2'}/>
+      <form action="">
+        {/**Упрвляемый элемент */}
+        <MyInput
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        {/**Неупрвляемый элемент */}
+        <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost} >Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={'The posts list 1'} />
+      <PostList posts={posts_2} title={'The posts list 2'} />
     </div>
   );
 }
