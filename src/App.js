69dfc126dc58163d3cf5da7bcd9917a252c1b0ba
GummerD <denis_gummer@mail.ru>
2023-12-25 12:08:21 +0300

    2.6. Сортировка постов.
    - сортировка потосв реализовна через тег select, который модифицировали
    в отдельный UI-элемент.
    
    - прокинули дефолтный пропс - defaultValue для одного тега - option,
    чтобы он отрисовывал значение сути селекта - соритровка.
    
    - в Аpp. создали props - options, который содержит в себе объект
    с дефолтными поисковыми значениями: сортировка по названию и сортировка
    по описанию, которые можно выбрать через селект. Далее этот пропс прокинули
    в MySelect и итерировали через map().
    
    - чтобы сортировать посты по задынным условиям, создали еще два пропса в
    селектe и новое состояние - selectedSort, для отслеживания выбора
    пользовтеля:
        value={setSelectedSort} -
        onChange={sortPost}
    передали их в наш селект. Таже, чтобы сортировать массив - posts, создали
    функцию- sortPost(sort).
    
    - в нашем select приняли просы value and onChange, организова по факту
    callback - onChange={event=>onChange(event.target.value)}, передав
    в sortPost(sort) - выбранное пользовтелем значение.
    
    - в sortPost(sort) через реструктуризации создаликопию массива - posts
    [...posts] и сравнили необходимые значение черех функцию sort() +
    localeCompare()

--- src/App.js
+++ src/App.js
@@ -2,43 +2,63 @@ import React, { useRef, useState } from "react";
 import PostList from "./components/PostList.jsx";
 import './style/App.css';
 import CreateNewPost from "./components/CreateNewPost.jsx";
+import MySelect from "./components/UI/select/MySelect.jsx";
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: "Some title 1", description: "Some description 1" },
-    { id: 2, title: "Some title 2", description: "Some description 2" },
-    { id: 3, title: "Some title 3", description: "Some description 3" },
-    { id: 4, title: "Some title 4", description: "Some description 4" },
-    { id: 5, title: "Some title 5", description: "Some description 5" },
+    { id: 2, title: "Some title 3", description: "Some description 2" },
+    { id: 3, title: "Some title 2", description: "Some description 3" },
+    { id: 4, title: "Some title 5", description: "Some description 4" },
+    { id: 5, title: "Some title 4", description: "Some description 5" },
     { id: 6, title: "Some title 6", description: "Some description 6" },
   ]);
 
-  function createPost(newPost){
+  const [selectedSort, setSelectedSort] = useState('');
+
+  function createPost(newPost) {
     setPosts([...posts, newPost])
   }
 
-  function deletePost(post){
-    setPosts(posts.filter(p=>p.id !== post.id))
+  function deletePost(post) {
+    setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  function sortPost(sort){
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])));
+  }
 
   return (
     <div className="App">
-      <CreateNewPost create = {createPost}/>
+      <CreateNewPost create={createPost} />
+      <hr style={{ margin: "15px 0" }} />
+      <div>
+        <MySelect 
+          value={setSelectedSort}
+          onChange={sortPost}
+          options = {[
+            {value: 'title', name: 'По названию'},
+            {value: 'description', name: 'По описанию'}
+          ]}
+          defaultValue='Сортировка'
+        />
+      </div>
       {/**Условная отрисовка */}
-      { posts.length !== 0 
+      {posts.length !== 0
         ?
-        <PostList deletePost ={deletePost} posts={posts} title={'The posts list 1'} />
+        <PostList deletePost={deletePost} posts={posts} title={'The posts list 1'} />
         :
         <div style={{
           fontSize: '25px',
-          color: 'red', 
-          textAlign: 'center', 
+          color: 'red',
+          textAlign: 'center',
           marginTop: '20px',
-          border: '1px solid red'}}>Посты не найдены!
+          border: '1px solid red'
+        }}>Посты не найдены!
         </div>
       }
-      
+
     </div>
   );
 }
