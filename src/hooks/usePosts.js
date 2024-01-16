1b8e5ba5ac86e10f7eceed311a1ceb3b4b4035ae
GummerD <denis_gummer@mail.ru>
2024-01-16 15:40:53 +0300

    5.2. Еще немного декомпозиции хуков.
    Выделили из usePosts() хук - useSortedPosts() в отдельный
    файл, так как react ругался на двойно export хуков внутри одного файла.

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -1,16 +1,5 @@
 import {useMemo} from "react"
-
-function useSortedPosts (posts, sort){
-  const sortedPosts = useMemo(() => {
-    if (sort) {
-      return [...posts].sort((a, b) => a[sort].localeCompare(b[sort]))
-    }
-    return posts
-  }, [posts, sort]);
-
-  return sortedPosts
-}
-
+import useSortedPosts from "./useSortedPosts"
 
 export default function usePosts (posts, sort, query) {
   const sortedPosts = useSortedPosts(posts, sort,);
